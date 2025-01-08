937c229cbf5647469e111466e5b5c822482488b5
Colleen-L <nonexistingalien@gmail.com>
2025-01-07 19:09:50 -0500

    transition

--- react-app-personal-portfolio/src/Transition.jsx
+++ react-app-personal-portfolio/src/Transition.jsx
@@ -1,30 +1,143 @@
+import React from 'react';
+import Slider from "react-slick";
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
+const imagesOne = [
+    {
+        img: "./css.png",
+        description: "CSS",
+    },
+    {
+        img: "./dart.png",
+        description: "Dart",
+    },
+    {
+        img: "./django.png",
+        description: "Django",
+    },
+    {
+        img: "./flutter.png",
+        description: "Flutter",
+    },
+    {
+        img: "./github.png",
+        description: "Github",
+    },
+    {
+        img: "./html.png",
+        description: "HTML",
+    },
+    {
+        img: "./intellij.png",
+        description: "IntelliJ",
+    },
+];
+
+const imagesTwo = [
+    {
+        img: "./java.png",
+        description: "Java",
+    },
+    {
+        img: "./javascript.png",
+        description: "JavaScript",
+    },
+    {
+        img: "./jupyter.png",
+        description: "Jupyter",
+    },
+    {
+        img: "./language.png",
+        description: "Multilingual",
+    },
+    {
+        img: "./python.png",
+        description: "Python",
+    },
+    {
+        img: "./react.png",
+        description: "React",
+    },
+    {
+        img: "./vscode.png",
+        description: "VSCode",
+    },
+];
+
 function Transition() {
+    const settings = {
+        infinite: true,
+        speed: 1000,
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 300,
+        dots: false,
+        arrows: false,
+        cssEase: 'ease-out',
+        swipeToSlide: true,
+    };
 
     return(
-       <div className="transition">
-         <div className="firstBar">
-            <div className = "list">
-                <div className = "item"><img src="./css.png" alt=""></img></div>
-                <div className = "item"><img src="./dart.png" alt=""></img></div>
-                <div className = "item"><img src="./django.png" alt=""></img></div>
-                <div className = "item"><img src="./flutter.png" alt=""></img></div>
-                <div className = "item"><img src="./github.png" alt=""></img></div>
-                <div className = "item"><img src="./html.png" alt=""></img></div>
-                <div className = "item"><img src="./intellij.png" alt=""></img></div>
-            </div>
-         </div>
-         <div className="secondBar">
-            <div className="list">
-                <div className = "item"><img src="./java.png" alt=""></img></div>
-                <div className = "item"><img src="./javascript.png" alt=""></img></div>
-                <div className = "item"><img src="./jupyter.png" alt=""></img></div>
-                <div className = "item"><img src="./language.png" alt=""></img></div>
-                <div className = "item"><img src="./python.png" alt=""></img></div>
-                <div className = "item"><img src="./react.png" alt=""></img></div>
-                <div className = "item"><img src="./vscode.png" alt=""></img></div>
-            </div>
-         </div>
-       </div>
+      <div className="container">
+         <div className = "slider">
+            <Slider {...settings}>
+                {imagesOne.map((item) => (
+                    <div className="cards">
+                        <div className="card" style={{
+                            width: '100px',
+                            margin: '10px',
+                            display: 'flex',
+                            flexDirection: 'column',
+                            textAlign: 'center',
+                            alignItems: 'center',
+                        }}>
+                            <img src={item.img} alt="" 
+                            style={{
+                                width: '40px',
+                            }}></img>
+                            <p style={{
+                                fontSize: '12px',
+                                color: 'black',
+                                textDecoration: 'bolder',
+                            }}> {item.description} </p>
+                        </div>
+                    </div>
+                ))}
+            </Slider>
+        </div>
+        <div className = "slider">
+            <Slider {...settings}>
+                    {imagesTwo.map((item) => (
+                        <div className="cards">
+                            <div className="card" style={{
+                                width: '100px',
+                                margin: '10px',
+                                display: 'flex',
+                                flexDirection: 'column',
+                                textAlign: 'center',
+                                alignItems: 'center',
+                            }}>
+                                <img src={item.img} alt="" 
+                                style={{
+                                    width: '40px',
+                                }}></img>
+                                <p style={{
+                                    fontSize: '12px',
+                                    color: 'black',
+                                    textDecoration: 'bolder',
+                                }}> {item.description} </p>
+                            </div>
+                        </div>
+                    ))}
+                </Slider>
+        </div>
+        <div className="arrows">
+            <img className="arrowOne" src="./down.png"></img>
+            <img className="arrowTwo" src="./down.png"></img>
+        </div>
+      </div>
     );
 
 }
